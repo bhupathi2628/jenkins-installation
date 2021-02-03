9a8170599046d1662cbc4d9cfb518794b511541f
bhupathi2628 <77353897+bhupathi2628@users.noreply.github.com>
2021-02-03 12:44:33 +0530

    Add files via upload

---
+++ install_jenkins.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#installing jenkins on amazon-linux 2
+
+sudo amazon-linux-extras install java-openjdk11
+
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
+
+sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
+
+sudo yum install jenkins
+
+sudo systemctl start jenkins
+
+sudo systemctl enable jenkins
+
+systemctl status jenkins
